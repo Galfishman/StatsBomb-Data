e4c4417662f3efbd08e8824d7eb1203e6ed44ee2
Galfishman <galfishman8@gmail.com>
2024-04-30 13:53:20 +0300

    Update StatsBomb.py

--- StatsBomb.py
+++ StatsBomb.py
@@ -607,9 +607,8 @@ def main():
             
             st.pyplot(fig)
 #####################################################################################################################################################################
-            df_shots = df_shots
             if PasserPick != "All":
-                    df_shots = df_shots[df_shots['player_name'] == PasserPick]
+                df_shots = df_shots[df_shots['player_name'] == PasserPick]
 
             df_goals = df_shots[(df_shots['outcome_name'] == "Goal")]
             df_ont = df_shots[(df_shots['outcome_name'] == "Saved")]
